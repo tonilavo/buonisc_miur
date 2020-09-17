1f0576f80de7cc767f0db5bc3abc81fed8de6c27
Tonino <tonino.lavorati@gmail.com>
2020-09-17 09:23:18 +0200

    urls added

--- front/urls.py
+++ front/urls.py
@@ -10,4 +10,13 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
 	path('api/', include(router.urls)),
+#	path('sitofermo', views.sito_fermo),
+	path('predomanda/', views.preform_insert),
+	path('update_domanda/<int:id>/', views.update_domanda),
+	path('review_domanda/<int:id>/', views.review_domanda),
+	re_path(r'^domanda/$', views.insert_domanda),
+	path('domandatest/',  views.domandatest),
+ 	path('upload/<int:id>/', views.upload_file),
+	path('msgfinale/<int:id>', views.msgfinale),
+	path('clear_files/<int:id>/', views.clear_database, name='clear_database'),
 ]
